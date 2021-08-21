84a6dfddc1a09a69357514615c7a17bb0df0871f
SoniaSu <frutos.rojos17@gmail.com>
2021-08-21 00:37:36 -0500

    getting tweets

--- Database/api_keys.py
+++ Database/api_keys.py
@@ -1,5 +1,5 @@
-# OpenWeatherMap API Key
-weather_api_key = "e9882b9f8db42ecddfea5b4b5cb589b3"
-
-# Google API Key
-g_key = "AIzaSyA3VM9ibHH-xfC99GhJ1dtqt8bKL9zejU0"
+# Twitter
+consumer_key = "jCruB2URIpOgvSUtcDEHwawrl"
+consumer_secret = "qHr73xJuXdilVD3f2QS7mlpCtMnfb1lx5JnFROcKsmDMcjtu4j"
+access_token = "16157274-aHyDpy5SQ41HbzqwTZbMTV5Cyzebz4K3uttT9m6eB"
+access_secret = "vOmZ0suDKGloPVDcKhYnj1kr0K0yQXYgl2sdkhN87oYW1"
