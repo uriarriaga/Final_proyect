39a827da7aaf33632e649f17d372a9cbc121b8fb
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-08-25 15:04:24 -0500

    round3

---
+++ File_Generators/config.py
@@ -0,0 +1,5 @@
+consumer_key =  'jCruB2URIpOgvSUtcDEHwawrl'
+consumer_secret = 'qHr73xJuXdilVD3f2QS7mlpCtMnfb1lx5JnFROcKsmDMcjtu4j'
+access_token = '16157274-aHyDpy5SQ41HbzqwTZbMTV5Cyzebz4K3uttT9m6eB'
+access_secret = 'vOmZ0suDKGloPVDcKhYnj1kr0K0yQXYgl2sdkhN87oYW1'
+
