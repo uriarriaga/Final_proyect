9735f908337596427918fcbe68f4465b46c67094
Gustavo M <gusmaldonadob@gmail.com>
2021-08-25 22:26:17 -0500

    JS Code & HTML & JSON & Visualizations

---
+++ static/js/twitter_stacked.js
@@ -0,0 +1,60 @@
+function twitter_stacked(response) {
+    // console.log(response)
+
+    response.forEach(element => {
+        element.total = element.positive+element.negative+element.neutral
+        element.ppositive = element.positive/element.total
+        element.pnegative = element.negative/element.total
+        element.pneutral = element.neutral/element.total
+    });
+    
+    console.log(response)
+
+    var tracepositive = {
+        x: response.map(i => i.ppositive),
+        y: response.map(i => i.key_word),
+        name: 'Positive',
+        orientation: 'h',
+        marker: {
+            color: 'rgba(55,128,191,0.6)',
+            width: 1
+        },
+        type: 'bar'
+    };
+
+    var tracenegative = {
+        x: response.map(i => i.pnegative),
+        y: response.map(i => i.key_word),
+        name: 'Negative',
+        orientation: 'h',
+        type: 'bar',
+        marker: {
+            color: 'rgba(255,153,51,0.6)',
+            width: 1
+        }
+    };
+
+    var traceneutral = {
+        x: response.map(i => i.pneutral),
+        y: response.map(i => i.key_word),
+        name: 'Neutral',
+        orientation: 'h',
+        type: 'bar',
+        marker: {
+            color: 'rgba(55,120,88,0.6)',
+            width: 1
+        }
+    };
+
+    var data = [tracepositive, traceneutral, tracenegative];
+
+    var layout = {
+        title: 'Twitter Stacked',
+        barmode: 'stack'
+    };
+
+    Plotly.newPlot('twitter_stacked', data, layout);
+
+};
+
+d3.json("/static/js/twitter_stacked.json").then(twitter_stacked);
