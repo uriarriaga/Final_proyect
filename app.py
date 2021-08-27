9d927059fe2bf2cbc2952997bf9ff61755a421db
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-08-26 19:34:43 -0500

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:9tckay8Bv^9e@database
 db = SQLAlchemy(app)
 db.reflect()
 
-class tweet(db.Model):
+class tweetdb(db.Model):
     __tablename__ = 'tweet'
 
 class sentiment(db.Model):
@@ -21,37 +21,14 @@ def index():
 
 @app.route("/tweet_share")
 def tweet_share():
-    response = [
-    {
-        "key_word": "Netflix",
-        "total_count": 700000
-    },
-    {
-        "key_word": "Amazon Prime Video",
-        "total_count": 300000
-    },
-    {
-        "key_word": "HBO Max",
-        "total_count": 100000
-    },
-    {
-        "key_word": "Paramount+",
-        "total_count": 50000
-    },
-    {
-        "key_word": "Blim",
-        "total_count": 350000
-    },
-    {
-        "key_word": "Star Plus",
-        "total_count": 10000
-    },
-    {
-        "key_word": "Disney +",
-        "total_count": 500000
-    }
-]
-    return jsonify(response)
+    response = tweetdb.query.all()
+    tweets = []
+    for i in response:
+        dict = {}
+        dict["tweet"] = i.tweet
+        dict["key_word"] = i.key_word
+        tweets.append(dict)
+    return jsonify(dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
